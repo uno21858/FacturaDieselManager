23f4ee15b0dd095d8d59f6365d4db711a1401faf
uno21 <erick_sanar005@hotmail.com>
2025-01-14 01:13:21 -0600

    updated

--- Icons_rc.py
+++ Icons_rc.py
@@ -2,7 +2,6 @@
 # Created by: object code
 # Created by: The Resource Compiler for Qt version 6.8.1
 # WARNING! All changes made in this file will be lost!
-
 from PySide6 import QtCore
 
 qt_resource_data = b"\
